255ea2f1641aaeb8dae753a3e82004b68b3bcab4
nacho98760 <nacho987600@gmail.com>
2025-10-03 20:52:55 -0300

    3/10/25

--- TP/main.py
+++ TP/main.py
@@ -1,84 +1,42 @@
 import json
+import random
 
-lista_de_eventos = ["Copa algorítmica", "Partido de Fútbol", "UADE Esports"]
-diccionario_de_usuarios = {}
+def imprimir_opciones(lista_de_opciones: list):
+    for x, evento in enumerate(lista_de_opciones):
+        print(str(x + 1) + ". " + evento)
 
-
-def main():
-
-    print(diccionario_de_usuarios)
+def validar_evento(lista_de_eventos: list):
     evento_valido = False
-    id_valido = False
-    edad_valida = False
 
     while evento_valido == False:
         try:
-            print("1. Copa algorítmica")
-            print("2. Partido de fútbol")
-            print("3. UADE Esports")
+            imprimir_opciones(lista_de_eventos)
             evento_elegido = int(input("Elija un evento al que asistir: "))
 
             while evento_elegido < 1 or evento_elegido > 3:
                 print("Numero de evento invalido. Intente nuevamente")
-                print("1. Copa algorítmica")
-                print("2. Partido de fútbol")
-                print("3. UADE Esports")
+                imprimir_opciones(lista_de_eventos)
                 evento_elegido = int(input("Elija un evento al que asistir: "))
 
-            match evento_elegido: 
-                case 1:
-                    evento_valido = True
-                    print("Evento de 'Copa algorítmica' elegido")
-                case 2:
-                    evento_valido = True
-                    print("Evento 'Partido de fútbol' elegido")
-                case 3:
-                    evento_valido = True
-                    print("Evento de 'UADE Esports' elegido")
+            evento_valido = True
+            return lista_de_eventos[evento_elegido - 1]
 
         except ValueError:
             print("Valor ingresado no valido")
 
 
-    while id_valido == False:
-        try:
-            id = int(input("Ingrese su ID (numero del 1 al 9999): "))
-
-            while id < 1 or id > 9999:
-                print("Numero de id no valido. Intente nuevamente.")
-                id = int(input("Ingrese su id (numero del 1 al 9999): "))
-
-            id_ya_registrado = False
-
-            with open("log.json", "r", encoding="utf-8", newline='') as archivo_de_guardado_json:
-
-                data = json.load(archivo_de_guardado_json)
-                
-                for item in data:
-                    if str(id) == item:
-                       id_ya_registrado = True
-                
-                while id_ya_registrado == True or id < 1 or id > 9999:
-                    print("Numero de id ya registrado o inválido. Intente nuevamente.")
-                    id_ya_registrado = False
-                    id = int(input("Ingrese su id (numero del 1 al 9999): "))
-
-                    for item in data:
-                        if str(id) == item:
-                            id_ya_registrado = True
-
-            id_valido = True
-
-        except ValueError:
-            print("Valor ingresado no valido. Intente nuevamente.")   
+def validar_nombre():
+    nombre = input("Ingrese su nombre: ") 
     
-
-    nombre = input("Ingrese su nombre: ")
-    
-    while nombre.isdigit() or len(nombre) == 0:
+    while nombre.isdigit()  or len(nombre) == 0:
         print("Nombre invalido")
         nombre = input("Ingrese su nombre: ")
 
+    return nombre
+
+
+def validar_edad():
+    edad_valida = False
 
     while edad_valida == False:
         try:
@@ -89,23 +47,101 @@ def main():
                 edad = int(input("Ingrese su edad: "))
             
             edad_valida = True
+            return edad
             
         except ValueError:
             print("Valor no valido")
 
-    diccionario_de_usuarios[id] = {}
-    diccionario_de_usuarios.update({id: [nombre, edad, lista_de_eventos[evento_elegido - 1]]})
 
+def generar_id():
+    id_valido = False
+    id = random.randint(1, 9999)
 
-    with open("log.json", "r", encoding="utf-8", newline='') as archivo_de_guardado_json:
+    id_ya_registrado = False
+    
+    while id_valido == False:
+        archivo_de_guardado_json = open("log.json", "r", encoding="utf-8", newline='')
+
         data = json.load(archivo_de_guardado_json)
+        for item in data:
+            if str(id) == item:
+                id_ya_registrado = True
+                    
+        while id_ya_registrado == True:
+            id_ya_registrado = False
+            id = random.randint(1, 9999)
 
-        data[str(id)] = diccionario_de_usuarios[id]
+            for item in data:
+                if str(id) == item:
+                    id_ya_registrado = True
+
+        id_valido = True
+        archivo_de_guardado_json.close()
+
+    return id
+
+
+def main_usuario():
+    lista_de_eventos = ["Copa algorítmica", "Partido de Fútbol", "UADE Esports"]
+    diccionario_de_usuarios = {}
+
+    evento_elegido = validar_evento(lista_de_eventos)
+    nombre = validar_nombre()
+    edad = validar_edad()
+    id = generar_id()
+
+    print("Tu numero de identificación es " + str(id))
+
+    diccionario_de_usuarios[id] = {}
+    diccionario_de_usuarios.update({id: [nombre, edad, evento_elegido]})
+
+    archivo_de_guardado_json = open("log.json", "r+", encoding="utf-8", newline='')
+    data = json.load(archivo_de_guardado_json)
+    data[str(id)] = diccionario_de_usuarios[id]
+    archivo_de_guardado_json.close()
+    
+    archivo_de_guardado_json_nuevo = open("log.json", "r+", encoding="utf-8", newline='')
+    json.dump(data, archivo_de_guardado_json_nuevo, indent=4, ensure_ascii=False)
+    archivo_de_guardado_json_nuevo.close()
+
+
+def validar_contraseña_de_admin():
+    contraseña_admin: int = 1234
+
+    contraseña = int(input("Ingrese la contraseña de administrador (Si ingreso al modo administrador por error, ingrese '0' para volver atras): "))
+
+    while contraseña != contraseña_admin:
+        if contraseña == 0:
+            return False
         
-        with open("log.json", "w", encoding="utf-8") as archivo_de_guardado_json:
-            json.dump(data, archivo_de_guardado_json)
+        print("Contraseña incorrecta. Por favor, intente nuevamente.")
+        contraseña = int(input("Ingrese la contraseña de administrador (Si ingreso al modo administrador por error, ingrese '0' para volver atras): "))
 
-            
-main()
+    return True
+
+def main_admin():
+    if validar_contraseña_de_admin():
+        print("Contraseña valida")
+    else:
+        elegir_modo_para_entrar()
 
 
+def elegir_modo_para_entrar():
+    print("----------------------------")
+    print("1. Ingresar como Usuario")
+    print("2. Ingresar como Administrador")
+    modo_elegido = int(input("Elija un modo para ingresar: "))
+
+    while modo_elegido < 1 or modo_elegido > 2:
+        print("El modo elegido es inválido. Por favor, intente nuevamente.")
+        print("1. Ingresar como Usuario")
+        print("2. Ingresar como Administrador")
+        modo_elegido = int(input("Elija un modo para ingresar: "))
+    
+    if modo_elegido == 1:
+        main_usuario()
+    else:
+        main_admin()
+
+
+elegir_modo_para_entrar()

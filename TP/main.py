7834bea6a8e9024fba19cd2eb981279e45966fce
nacho98760 <nacho987600@gmail.com>
2025-10-13 21:16:06 -0300

    13/10

--- TP/main.py
+++ TP/main.py
@@ -1,6 +1,13 @@
 import json
 import random
 
+
+def devolver_lista_de_eventos():
+    lista_de_eventos = ["Copa algorítmica", "Partido de Fútbol", "UADE Esports"]
+    return lista_de_eventos
+
+
+
 def imprimir_opciones(lista_de_opciones: list):
     for x, evento in enumerate(lista_de_opciones):
         print(str(x + 1) + ". " + evento)
@@ -82,7 +89,7 @@ def generar_id():
 
 
 def main_usuario():
-    lista_de_eventos = ["Copa algorítmica", "Partido de Fútbol", "UADE Esports"]
+    lista_de_eventos = devolver_lista_de_eventos()
     diccionario_de_usuarios = {}
 
     evento_elegido = validar_evento(lista_de_eventos)
@@ -126,6 +133,7 @@ def ver_registros_completos():
 
     for x, item in enumerate(data):
         print("Usuario " + str(x + 1) + ":")
+        print("ID: " + item)
         print("Nombre: " + data[item][0])
         print("Edad: " + str(data[item][1]))
         print("Evento al que asistío: " + data[item][2])
@@ -146,14 +154,14 @@ def ver_registro_especifico():
             for item in data:
                 if str(id_elegido) == item:
                     id_valido = True
-                    break     
+                    break   
             
-            print("El id no se ha encontrado.")
+            if id_valido == False:
+                print("El id no se ha encontrado.")
 
         except ValueError:
             print("El valor ingresado no es valido.")
 
-    
     print("Usuario encontrado")
     print("Nombre: " + data[str(id_elegido)][0])
     print("Edad: " + str(data[str(id_elegido)][1]))
@@ -168,31 +176,101 @@ def ver_registro_especifico():
 
     while opcion_valida == False:
         try:
-            opcion_elegida = int(input("Elija una opcion de las anteriores"))
+            opcion_elegida = int(input("Elija una opcion de las anteriores: "))
 
             while opcion_elegida < 1 or opcion_elegida > len(datos_del_usuario):
                 print("Opcion no valida. Por favor, intente nuevamente.")
                 mostrar_opciones_de_modificacion(datos_del_usuario)
-                opcion_elegida = int(input("Elija una opcion de las anteriores"))
+                opcion_elegida = int(input("Elija una opcion de las anteriores: "))
             
-            #--------------------------------------------------------------------------------------------------------------------------------
-            #--------------------------------------------------------------------------------------------------------------------------------
-            #--------------------------------------------------------------------------------------------------------------------------------
-            #--------------------------------------------------------------------------------------------------------------------------------
-            #--------------------------------------------------------------------------------------------------------------------------------
-            #--------------------------------------------------------------------------------------------------------------------------------
-
+            opcion_valida = True
+            buscar_dato_a_modificar(opcion_elegida, id_elegido)
 
         except ValueError:
             print("El valor ingresado no es valido. Por favor, intente nuevamente.")
     
 
+    archivo_json.close()
+
+
+def buscar_dato_a_modificar(opcion_elegida, id_elegido):
+    archivo_json = open("log.json", "r+", encoding="utf-8", newline='')
+    data = json.load(archivo_json)
+
+    match opcion_elegida:
+        case 1:
+            modificar_nombre(id_elegido, data)
+        case 2:
+            modificar_edad(id_elegido, data)
+        case 3:
+            modificar_evento(id_elegido, data)
+    
+    archivo_json.close()
+
+    archivo_json_actualizado = open("log.json", "w", encoding="utf-8", newline='')
+    json.dump(data, archivo_json_actualizado, indent=4, ensure_ascii=False)
+    archivo_json_actualizado.close()
+
+
+def modificar_nombre(id_elegido, data):
+    nuevo_nombre = input("Ingrese un nuevo nombre para reemplazar al existente: ")
+    
+    if (nuevo_nombre.isdigit() or nuevo_nombre == ""): return
+
+    data[str(id_elegido)][0] = nuevo_nombre
+
+
+def modificar_edad(id_elegido, data):
+    edad_valida = False
+    while edad_valida == False:
+        try:
+            nueva_edad = int(input("Ingrese un nuevo valor para reemplazar al existente: "))
+
+            while nueva_edad < 13 or nueva_edad > 100:
+                print("Edad ingresada no válida.")
+                nueva_edad = int(input("Ingrese un nuevo valor para reemplazar al existente: "))
+
+            edad_valida = True
+
+        except ValueError:
+            print("El valor ingresado no es válido. Por favor, intente nuevamente.")
+
+    data[str(id_elegido)][1] = nueva_edad
+
+
+def modificar_evento(id_elegido, data):
+    lista_de_eventos_actual = devolver_lista_de_eventos()
+
+    evento_valido = False
+    while evento_valido == False:
+        try:
+            print("--------------------------------------------")
+            for x, i in enumerate(lista_de_eventos_actual):
+                print(str(x + 1) + ". " + i)
+            nuevo_evento = int(input("Elija un nuevo evento de las opciones anteriores para reemplazar al existente: "))
+
+            while nuevo_evento < 1 or nuevo_evento > len(lista_de_eventos_actual):
+                print("Opcion elegida no válida. Por favor, intente nuevamente.")
+                print("--------------------------------------------")
+                for x, i in enumerate(lista_de_eventos_actual):
+                    print(str(x + 1) + ". " + i)
+                nuevo_evento = int(input("Elija un nuevo evento de las opciones anteriores para reemplazar al existente: "))
+
+            evento_valido = True
+
+        except ValueError:
+            print("El valor ingresado no es válido. Por favor, intente nuevamente.")
+
+    data[str(id_elegido)][2] = lista_de_eventos_actual[nuevo_evento - 1]
+
+        
 
 def mostrar_opciones_de_modificacion(datos_del_usuario):
     for x in range(len(datos_del_usuario)):
         print(str(x + 1) + ". " + "Modificar " + datos_del_usuario[x] + " del usuario")
 
     print(str(len(datos_del_usuario) + 1) + ". Salir") 
+
 
 def main_admin():
     if validar_contraseña_de_admin():

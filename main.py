0f4b7d2c71e04ce622321467af4992ddc896cbf9
nacho98760 <nacho987600@gmail.com>
2025-10-28 11:49:46 -0300

    27/10

--- main.py
+++ main.py
@@ -61,11 +61,11 @@ def validar_evento(lista_de_eventos: list):
 
     archivo_de_eventos_y_entradas_json_actualizado.close()
 
-    return lista_de_eventos[evento_elegido - 1]
+    return lista_de_eventos[evento_elegido - 1], cantidad_de_entradas_elegida
 
 
 def verificar_cantidad_de_entradas():
-    cantidad_maxima_de_entradas = 10
+    cantidad_maxima_de_entradas = 9
 
     entrada_valida = False
 
@@ -144,7 +144,7 @@ def main_usuario():
     lista_de_eventos = devolver_lista_de_datos(0)
     diccionario_de_usuarios = {}
 
-    evento_elegido = validar_evento(lista_de_eventos)
+    evento_elegido, cantidad_de_entradas_elegida = validar_evento(lista_de_eventos)
     nombre = validar_nombre()
     edad = validar_edad()
     id = generar_id()
@@ -152,7 +152,7 @@ def main_usuario():
     print("Tu numero de identificación es " + str(id))
 
     diccionario_de_usuarios[id] = {}
-    diccionario_de_usuarios.update({id: [nombre, edad, evento_elegido]})
+    diccionario_de_usuarios.update({id: [nombre, "Edad: " + str(edad), evento_elegido, "Entradas: " + str(cantidad_de_entradas_elegida)]})
 
     archivo_de_guardado_json = open("log.json", "r+", encoding="utf-8", newline='')
     data = json.load(archivo_de_guardado_json)
@@ -220,7 +220,7 @@ def ver_registro_especifico():
     print("Evento al que asistio: " + data[str(id_elegido)][2])
     print("---------------------")
 
-    datos_del_usuario = ["nombre", "edad", "evento"]
+    datos_del_usuario = ["nombre", "edad", "evento", "entradas"]
 
     mostrar_opciones_de_modificacion(datos_del_usuario)
     
@@ -236,10 +236,11 @@ def ver_registro_especifico():
                 opcion_elegida = int(input("Elija una opcion de las anteriores: "))
             
             opcion_valida = True
-            buscar_dato_a_modificar(opcion_elegida, id_elegido)
 
         except ValueError:
             print("El valor ingresado no es valido. Por favor, intente nuevamente.")
+        
+    buscar_dato_a_modificar(opcion_elegida, id_elegido)
     
 
     archivo_json.close()
@@ -256,6 +257,8 @@ def buscar_dato_a_modificar(opcion_elegida, id_elegido):
             modificar_edad(id_elegido, data)
         case 3:
             modificar_evento(id_elegido, data)
+        case 4:
+            modificar_entradas(id_elegido, data)
     
     archivo_json.close()
 
@@ -287,7 +290,7 @@ def modificar_edad(id_elegido, data):
         except ValueError:
             print("El valor ingresado no es válido. Por favor, intente nuevamente.")
 
-    data[str(id_elegido)][1] = nueva_edad
+    data[str(id_elegido)][1] = "Edad: " + str(nueva_edad)
 
 
 def modificar_evento(id_elegido, data):
@@ -315,8 +318,52 @@ def modificar_evento(id_elegido, data):
 
     data[str(id_elegido)][2] = lista_de_eventos_actual[nuevo_evento - 1]
 
-        
 
+def modificar_entradas(id_elegido, data):
+    cantidad_de_entradas_valida = False
+    while cantidad_de_entradas_valida == False:
+        try:
+            nueva_cantidad_de_entradas = int(input("Ingrese una nueva cantidad de entradas para reemplazar a la existente: "))
+
+            while nueva_cantidad_de_entradas < 1 or nueva_cantidad_de_entradas > 9:
+                print("Cantidad de entradas ingresada no válida.")
+                nueva_cantidad_de_entradas = int(input("Ingrese una nueva cantidad de entradas para reemplazar a la existente: "))
+
+            cantidad_de_entradas_valida = True
+
+        except ValueError:
+            print("El valor ingresado no es válido. Por favor, intente nuevamente.")
+
+    numero_de_entradas_encontrado = None
+
+    for i in data[str(id_elegido)][3]:
+        if i.isdigit():
+           numero_de_entradas_encontrado = i
+           break
+
+    data[str(id_elegido)][3] = "Entradas: " + str(nueva_cantidad_de_entradas)
+
+    cantidad_de_entradas_restantes_actualizada = int(numero_de_entradas_encontrado) - nueva_cantidad_de_entradas
+
+    lista_de_eventos = devolver_lista_de_datos(0)
+
+    index_de_evento_elegido = lista_de_eventos.index(data[str(id_elegido)][2])
+
+    archivo_de_eventos_y_entradas_json = open("eventos_y_entradas.json", "r", encoding="utf-8", newline='')
+    data_eventos_entradas = json.load(archivo_de_eventos_y_entradas_json)
+
+    for i in data_eventos_entradas:
+        data_eventos_entradas[i][index_de_evento_elegido][1] += cantidad_de_entradas_restantes_actualizada
+    
+
+    archivo_de_eventos_y_entradas_json.close()
+
+    archivo_de_eventos_y_entradas_json = open("eventos_y_entradas.json", "w", encoding="utf-8", newline='')
+    
+    json.dump(data_eventos_entradas, archivo_de_eventos_y_entradas_json, indent=4, ensure_ascii=False)
+    archivo_de_eventos_y_entradas_json.close()
+
+        
 def mostrar_opciones_de_modificacion(datos_del_usuario):
     for x in range(len(datos_del_usuario)):
         print(str(x + 1) + ". " + "Modificar " + datos_del_usuario[x] + " del usuario")
@@ -324,34 +371,98 @@ def mostrar_opciones_de_modificacion(datos_del_usuario):
     print(str(len(datos_del_usuario) + 1) + ". Salir") 
 
 
+def imprimir_opciones_para_administrador(lista_de_opciones_de_administrador):
+    for x, item in enumerate(lista_de_opciones_de_administrador):
+        print(str(x + 1) + ". " + item)
+
+
+def elegir_opcion_para_administador(opcion_elegida):
+    funciones_para_ejecutar = {1: ver_registros_completos, 2: ver_registro_especifico, 3: agregar_evento, 4: remover_evento}
+
+    funciones_para_ejecutar[opcion_elegida]()
+
+
+def agregar_evento():
+    archivo_de_eventos_y_entradas_json = open("eventos_y_entradas.json", "r", encoding="utf-8", newline='')
+    data = json.load(archivo_de_eventos_y_entradas_json)
+
+    evento = input("Ingrese un nombre para el evento a agregar: ")
+
+    while evento.isdigit() or evento == "":
+        print("Nombre no válido.")
+        evento = input("Ingrese un nombre para el evento a agregar: ")
+
+    for i in data:
+        data[i].append([evento, 250])
+    
+    archivo_de_eventos_y_entradas_json.close()
+
+    archivo_de_eventos_y_entradas_json_actualizado = open("eventos_y_entradas.json", "w", encoding="utf-8", newline='')
+    json.dump(data, archivo_de_eventos_y_entradas_json_actualizado, indent=4, ensure_ascii=False)
+    archivo_de_eventos_y_entradas_json_actualizado.close()
+
+
+def remover_evento():
+    lista_de_eventos = devolver_lista_de_datos(0) # ["Copa algoritimica", "Partido de futbol", "UADE Esports"]
+
+    evento_valido = False
+
+    while evento_valido == False:
+        try:
+            print("---------------------------------------")
+            imprimir_opciones(lista_de_eventos)
+            evento_a_remover = int(input("Elija el evento que desea remover: "))
+
+            while evento_a_remover < 1 or evento_a_remover > len(lista_de_eventos):
+                print("El evento elegido no existe. Por favor, intente de nuevo.")
+                print("---------------------------------------")
+                imprimir_opciones(lista_de_eventos)
+                evento_a_remover = int(input("Elija el evento que desea remover: "))
+                
+            evento_valido = True
+
+        except ValueError:
+            print("El valor ingresado no es válido. Por favor, intente nuevamente.")
+    
+    archivo_de_eventos_y_entradas_json = open("eventos_y_entradas.json", "r", encoding="utf-8", newline='')
+    data = json.load(archivo_de_eventos_y_entradas_json)
+    
+    lista_de_datos = data["eventos_y_entradas_restantes"]
+    lista_de_datos.remove(lista_de_datos[evento_a_remover - 1])
+    archivo_de_eventos_y_entradas_json.close()
+
+    archivo_de_eventos_y_entradas_json_actualizado = open("eventos_y_entradas.json", "w", encoding="utf-8", newline='')
+    json.dump(data, archivo_de_eventos_y_entradas_json_actualizado, indent=4, ensure_ascii=False)
+
+
+
 def main_admin():
+    lista_de_opciones_de_administrador = ["Ver registros completos de usuarios", "Modificar un registro de usuario", "Agregar evento", "Remover evento"]
+
     if validar_contraseña_de_admin():
         
-        print("1. Ver registros completos de usuarios")
-        print("2. Modificar un registro de usuario")
+        imprimir_opciones_para_administrador(lista_de_opciones_de_administrador)
         opcion_elegida = int(input("Elija una opcion para continuar: "))
 
         opcion_valida = False
 
         while opcion_valida == False:
             try:
-                while opcion_elegida < 1 or opcion_elegida > 2:
+                while opcion_elegida < 1 or opcion_elegida > len(lista_de_opciones_de_administrador):
                     print("Opción no válida. Por favor, intente nuevamente.")
-                    print("1. Ver registros completos de usuarios")
-                    print("2. Modificar un registro de usuario")
+                    imprimir_opciones_para_administrador(lista_de_opciones_de_administrador)
                     opcion_elegida = int(input("Elija una opcion para continuar: "))
                 
                 opcion_valida = True
-                if opcion_elegida == 1:
-                    ver_registros_completos()
-                else:
-                    ver_registro_especifico()
 
             except ValueError:
                 print("El valor ingresado no es válido. Por favor, intente nuevamente.")
+        
+        elegir_opcion_para_administador(opcion_elegida)
+
     else:
         elegir_modo_para_entrar()
-
+        
 
 def elegir_modo_para_entrar():
     print("----------------------------")
@@ -369,6 +480,5 @@ def elegir_modo_para_entrar():
         main_usuario()
     else:
         main_admin()
-
 
 elegir_modo_para_entrar()
